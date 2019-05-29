42dd2a1d68922fcfeb9e7ca15862a3bad483c1c4
cfigueirasvalverde <cfigueirasvalverde@nautilus26.danielcastelao.org>
2019-05-29 11:34:12 +0200

    Mi base de datos

--- src/sql/TablaEliminate.java
+++ src/sql/TablaEliminate.java
@@ -268,9 +268,6 @@ public class TablaEliminate extends javax.swing.JFrame {
         JOptionPane.showMessageDialog(null, "Alumno eliminado");
     }//GEN-LAST:event_btnBorrarActionPerformed
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String args[]) {
         /* Set the Nimbus look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
